105c9a05a3f4c2ee2c9ca1bec7a470b45e9f9c34
Brodrick George <brodrick.george@yahoo.com>
2020-10-16 08:48:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,5 @@
 def join_nested_strings(src)
-  just_the_strings = " "
+  just_the_strings = ""
   row_index = 0
   while row_index < src.count do element_index = 0 
     while element_index < src[row_index].count do
@@ -11,7 +11,7 @@ def join_nested_strings(src)
    
  row_index +=1
    end
-just_the_strings
+just_the_strings.join
  end
 
 
